b9566a75798302799b1fd84fd4ad52f3657e26eb
xbc <463449055@qq.com>
2016-05-12 17:55:59 +0800

    update

---
+++ js/common.js
@@ -0,0 +1,86 @@
+// ajax({
+//     url: "./TestXHR.aspx",              //请求地址
+//     type: "POST",                       //请求方式
+//     data: { name: "super", age: 20 },        //请求参数
+//     dataType: "json",
+//     success: function (response, xml) {
+//         // 此处放成功后执行的代码
+//     },
+//     fail: function (status) {
+//         // 此处放失败后执行的代码
+//     }
+// });
+function ajax(options) {
+    options = options || {};
+    options.type = (options.type || "GET").toUpperCase();
+    options.dataType = options.dataType || "json";
+    var params = formatParams(options.data);
+
+    //创建 - 非IE6 - 第一步
+    if (window.XMLHttpRequest) {
+        var xhr = new XMLHttpRequest();
+    } else { //IE6及其以下版本浏览器
+        var xhr = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    //接收 - 第三步
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var status = xhr.status;
+            if (status >= 200 && status < 300) {
+                options.success && options.success(xhr.responseText, xhr.responseXML);
+            } else {
+                options.fail && options.fail(status);
+            }
+        }
+    }
+
+    //连接 和 发送 - 第二步
+    if (options.type == "GET") {
+        xhr.open("GET", options.url + "?" + params, true);
+        xhr.send(null);
+    } else if (options.type == "POST") {
+        xhr.open("POST", options.url, true);
+        //设置表单提交时的内容类型
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(params);
+    }
+}
+//格式化参数
+function formatParams(data) {
+    var arr = [];
+    for (var name in data) {
+        arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
+    }
+    return arr.join('&');
+}
+
+
+// dom operator
+function hasClass(obj, cls) {  
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));  
+}  
+  
+function addClass(obj, cls) {  
+    if (!this.hasClass(obj, cls)) obj.className += " " + cls;  
+}  
+  
+function removeClass(obj, cls) {  
+    if (hasClass(obj, cls)) {  
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');  
+        obj.className = obj.className.replace(reg, ' ');  
+    }  
+}  
+  
+function toggleClass(obj,cls){  
+    if(hasClass(obj,cls)){  
+        removeClass(obj, cls);  
+    }else{  
+        addClass(obj, cls);  
+    }  
+}  
+  
+function toggleClassTest(){  
+    var obj = document. getElementById('test');  
+    toggleClass(obj,"testClass");  
+} 
